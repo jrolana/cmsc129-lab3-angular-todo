638fd86c023fab6f88e275432f7cb4a7dda9e819
jrolana <olanajhoanna@gmail.com>
2025-03-04 23:42:53 +0800

    [Setup][Olana] Header and Button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() color: string = "orange";
+  @Input() text: string = "Close";
+}
